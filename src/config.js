fd3cc4d1fcb7033cecace187f2615975347962a0
Nutti Saelor <nutti.sae@truedigital.com>
2022-05-01 21:38:13 +0700

    change contract

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || "donation.leelorz.testnet";
+const CONTRACT_NAME = process.env.CONTRACT_NAME || "donate.leelorz.testnet";
 
 function getConfig(env) {
   switch (env) {
